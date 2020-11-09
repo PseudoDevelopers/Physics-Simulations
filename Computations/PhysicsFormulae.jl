cc8632a2771c63b8a7c03bc4b7c7634739fbe129
PSEUDO <Pseudo.Developers@gmail.ccom>
2020-11-09 11:52:47 +0500

    Formula for bounce & combinedVelocity added
    Only the formulea added. But I've to write code to use these formulae.

--- Computations/PhysicsFormulae.jl
+++ Computations/PhysicsFormulae.jl
@@ -12,12 +12,25 @@ function acceleration(node1, node2)
     distance = sum(square.(displacement))
 
     dv = normalize(displacement)
-    dv = ((G * node2.mass) / square(distance)).*dv
+    dv = ((G * node2.mass) / square(distance)) .* dv
 
     return dv
 end
 
-square(n) = n * n
+function bounce(node1, node2)
+    m₁, m₂ = node1.mass, node2.mass
+    v₁, v₂ = node1.velocity, node2.velocity
+    
+    vf₁ = v₁ .* ((m₁ - m₂) / (m₂ + m₁))
+    vf₁ += v₂ .* ((2 * m₂) / (m₂ + m₁))
+
+    vf₂ = v₁ .* ((2 * m₁) / (m₂ + m₁))
+    vf₂ += v₂ .* ((m₂ - m₁) / (m₂ + m₁))
+
+    return vf₁, vf₂
+end
+
+square(n) = n^2
 
 export acceleration
 end
